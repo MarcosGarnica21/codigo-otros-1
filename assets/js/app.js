1bfc312a144b11b2db6c1b243bd10aa0ce898323
MarcosGarnica21 <garnicar_@hotmail.com>
2025-07-07 17:30:10 -0600

    Added comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,24 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('.name');
+const $n = document.querySelector('.name'); //Estos tres, no estaba siendo llamados correctamente, se debe llamar con "." porque son clases
 const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-async function displayUser(username) {
+async function displayUser(username) { //declar la función como async para usar await
   $n.textContent = 'cargando...';
-  try {
     const response = await fetch(`${usersEndpoint}/${username}`);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
     const data = await response.json();
     console.log(data);
-    $n.textContent = `${data.name}`;
+
+    $n.textContent = `${data.name}`; // no se estaban usando backticks
     $b.textContent = `${data.blog}`;
     $l.textContent = `${data.location}`;
-  } catch (error) {
-    handleError(error);
-  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.error(err);
-  n.textContent = `Algo salió mal: ${err.message}`
+  $n.textContent = `Algo salió mal: ${err.message}`; //.meesage para que muestre le mensaje y accede a $n
 }
 
 displayUser('stolinski').catch(handleError);
-
-console.log
